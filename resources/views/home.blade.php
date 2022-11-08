8de271ca6954476720859222301b88503bf01e46
Faraz Mundia <farazmuhammad1995@gmail.com>
2022-11-09 04:53:22 +0500

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,41 +5,41 @@
 
 </h6>
 <div class="py-5">
-    <div class="container">
-      <div class="row">
-        @can('role_access')
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-block">
-              <h4 class="card-title">Total Adminstrators</h4>
-              <h6 class="card-subtitle text-muted">6</h6>
-              <a href="{{route('admin.users.index')}}" class="card-link">View List</a>
-            </div>
+  <div class="container">
+    <div class="row">
+      @can('role_access')
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Total Adminstrators</h4>
+            <h6 class="card-subtitle text-muted">6</h6>
+            <a href="{{route('admin.users.index')}}" class="card-link">View List</a>
           </div>
-        </div>
-        @endcan
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-block">
-              <h4 class="card-title">Total Users</h4>
-              <h6 class="card-subtitle text-muted">6</h6>
-              <a href="{{route('admin.users.index')}}" class="card-link">View List</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-block">
-              <h4 class="card-title">Total Uploaded Files</h4>
-              <h6 class="card-subtitle text-muted">6</h6>
-              <a href="{{route('admin.documents.documentlist')}}" class="card-link">View List</a>
-            </div>
-          </div>
-        </div>
         </div>
       </div>
+      @endcan
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Total Users</h4>
+            <h6 class="card-subtitle text-muted">6</h6>
+            <a href="{{route('admin.users.index')}}" class="card-link">View List</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Total Uploaded Files</h4>
+            <h6 class="card-subtitle text-muted">6</h6>
+            <a href="{{route('admin.documents.index')}}" class="card-link">View List</a>
+          </div>
+        </div>
       </div>
     </div>
+  </div>
+</div>
+</div>
 <div class="mT-30"></div>
 @endsection
 @section('scripts')
