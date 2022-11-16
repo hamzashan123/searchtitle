30a007da49eaf55fd98eef09dbf203baa9fe732d
DESKTOP-UVB12RQ\Hamza <hamzashan123@gmail.com>
2022-11-16 01:10:39 -0800

    asd

--- resources/views/emails/emailtouser.blade.php
+++ resources/views/emails/emailtouser.blade.php
@@ -1,4 +1,4 @@
 ﻿<h1>Hello there,</h1>
 
 <p>This is the document email.</p>
-<p>{!! $documenturl!!}</p>
+<p>click this link to download the document <a href="{!! $documenturl!!}"> Download here</a></p>
